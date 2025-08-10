d2083e710088b07b07f1e09b991f1b2c5693c2b1
Mirian97 <mirianquispe1505@gmail.com>
2025-08-10 18:45:22 -0300

    feat: implement createEvent method in EventController to handle event creation

--- src/main/java/com/ebanx/bankingSystem/controller/EventController.java
+++ src/main/java/com/ebanx/bankingSystem/controller/EventController.java
@@ -1,16 +1,25 @@
 package com.ebanx.bankingSystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ebanx.bankingSystem.service.AccountService;
+import com.ebanx.bankingSystem.dto.EventResponseDTO;
+import com.ebanx.bankingSystem.dto.TransactionDTO;
+import com.ebanx.bankingSystem.service.TransactionService;
 
 @RestController
 @RequestMapping("/event")
 public class EventController {
 
   @Autowired
-  private AccountService accountService;
+  private TransactionService transactionService;
 
+  @PostMapping
+  public ResponseEntity<EventResponseDTO> createEvent(@RequestBody TransactionDTO dto) {
+    return ResponseEntity.ok().body(transactionService.create(dto));
+  }
 }
